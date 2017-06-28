59376ec141a95b6a7d94e31954e1e73d6ecbeef2
Gerson Reis <gersonapollo@gmail.com>
2017-06-28 01:08:20 -0300

    Iniciando inclusao de notificacao

--- app/src/main/java/br/com/chaos/auxiliarestudos/RevisoesActivity.java
+++ app/src/main/java/br/com/chaos/auxiliarestudos/RevisoesActivity.java
@@ -1,8 +1,14 @@
 package br.com.chaos.auxiliarestudos;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,6 +22,7 @@ import android.widget.ListView;
 import java.util.List;
 
 import br.com.chaos.auxiliarestudos.dao.RevisaoDao;
+import br.com.chaos.auxiliarestudos.helper.FormularioRevisaoHelper;
 import br.com.chaos.auxiliarestudos.modelo.Revisao;
 
 public class RevisoesActivity extends AppCompatActivity {
@@ -89,23 +96,47 @@ public class RevisoesActivity extends AppCompatActivity {
 
     }
 
-    //    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater menuInflater = getMenuInflater();
-//        menuInflater.inflate(R.menu.revisoes_menu, menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()){
-//            case R.id.menu_adicionar:
-//                Intent intent = new Intent(RevisoesActivity.this, FormularioRevisaoActivity.class);
-//                startActivity(intent);
-//                break;
-//        }
-//
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+    private void gerarNotificacao(){
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_confirmar)
+                .setContentTitle("Minha Notificacao")
+                .setContentText("Teste de Notificacao");
+
+        Intent resultIntent = new Intent(this, FormularioRevisaoActivity.class);
+
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(this);
+        taskStackBuilder.addParentStack(FormularioRevisaoActivity.class);
+
+        taskStackBuilder.addNextIntent(resultIntent);
+        PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        notificationBuilder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationBuilder.setVibrate(new long[] {100, 250, 100, 500});
+
+        notificationManager.notify(0, notificationBuilder.build());
+
+
+    }
+
+        @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.revisoes_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_adicionar:
+                gerarNotificacao();
+                break;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
